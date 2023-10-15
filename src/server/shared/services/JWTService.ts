ded66364b1e4a6d451c854523440bce4bb6ea768
robertocaliz <robertocaliz72@gmail.com>
2023-10-15 08:09:08 +0200

    feat: add jwt service

---
+++ src/server/shared/services/JWTService.ts
@@ -0,0 +1,42 @@
+
+import * as Jwt from 'jsonwebtoken';
+
+
+interface IUserData {
+	userId: number;
+}
+
+
+export enum EJWTError {
+	INVALID_TOKEN,
+	SECRET_NOT_FOUND
+}
+
+
+const jwtSecret = process.env.JWT_SECRET;
+
+
+const sign = (data: IUserData): string => {
+	if (!jwtSecret) throw new Jwt.JsonWebTokenError('Secret not found!');
+	return Jwt.sign(data, jwtSecret, { expiresIn: '1h' });
+};
+
+
+const verify = (token: string): IUserData | EJWTError.INVALID_TOKEN => {
+	if (!jwtSecret) throw new Jwt.JsonWebTokenError('Secret not found');
+	try {
+		const payload = Jwt.verify(token, jwtSecret);
+		if (typeof payload === 'object') {
+			return <IUserData>payload;
+		}
+		return EJWTError.INVALID_TOKEN;
+	} catch (error) {
+		return EJWTError.INVALID_TOKEN;
+	}
+};
+
+
+export const JWTService = {
+	sign,
+	verify
+};
