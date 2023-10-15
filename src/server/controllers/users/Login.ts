ae9fb9e1a1ddd7c7f12ab86f903dc8e6a4a965d5
robertocaliz <robertocaliz72@gmail.com>
2023-10-15 07:51:20 +0200

    alteration: change login action of users controller

--- src/server/controllers/users/Login.ts
+++ src/server/controllers/users/Login.ts
@@ -3,14 +3,14 @@ import { User } from '../../database/models';
 import { UsersProvider } from '../../database/providers';
 import { StatusCodes } from 'http-status-codes';
 import { UnauthorizedError } from '../../utils/errors';
+import { PasswordService } from '../../shared/services';
 
 
 
 export const login = async (req: Request<{}, {}, Pick<User, 'email' | 'password'>>, res: Response) => {
 	const { email, password } = req.body;
 	const user = await UsersProvider.getByEmail(email);
-	console.log(user);
-	if (user && user.password === password) {
+	if (user && await PasswordService.equals(password, user.password)) {
 		return res
 			.status(StatusCodes.OK)
 			.json(user);
