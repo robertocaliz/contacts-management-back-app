6ec9951ce1f11f27cc1789c0e9d1561af8dd4b31
robertocaliz <robertocaliz72@gmail.com>
2023-10-15 07:47:28 +0200

    alteration: change signup action to encrypt password

--- src/server/controllers/users/Signup.ts
+++ src/server/controllers/users/Signup.ts
@@ -2,12 +2,15 @@ import { Request, Response } from 'express';
 import { User } from '../../database/models';
 import { UsersProvider } from '../../database/providers';
 import { StatusCodes } from 'http-status-codes';
+import { PasswordService } from '../../shared/services';
+
 
 
 export const signup = async (req: Request<{}, {}, User>, res: Response) => {
 	const user = req.body;
+	const hash = await PasswordService.getHash(user.password);
 	await UsersProvider
-		.create(user)
+		.create({ ...user, password: hash })
 		.then(userId => {
 			res.status(StatusCodes.CREATED).json(userId);
 		});
