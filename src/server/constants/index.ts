b93622ff0bfaccdebda1484e943bd609caf7e0de
robertocaliz <robertocaliz72@gmail.com>
2023-10-15 12:13:23 +0200

    alteration: freeze objects

--- src/server/constants/index.ts
+++ src/server/constants/index.ts
@@ -1,17 +1,17 @@
 
 
 
-export const TABLE_NAMES = {
+export const TABLE_NAMES = Object.freeze({
 	users: 'users',
 	contacts: 'contacts'
-};
+});
 
 
 
 
-export const REGEX = {
+export const REGEX = Object.freeze({
 	db: {
 		email: '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$',
 		phoneNumber: '^(\\+258)?[28]\\d{8}$'
 	}
-};
+});
