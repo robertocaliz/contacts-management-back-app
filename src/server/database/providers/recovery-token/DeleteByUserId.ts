4b9727426e7c738e904e7e9bbf7563bcb2950eb6
robertocaliz <robertocaliz72@gmail.com>
2023-11-08 18:32:25 +0200

    recovery token provider: modify

--- src/server/database/providers/recovery-token/DeleteByUserId.ts
+++ src/server/database/providers/recovery-token/DeleteByUserId.ts
@@ -5,12 +5,7 @@ import { recoveryTokenModel } from '../../models';
 
 export const deleteByUserId = async (userId: string) => {
 	try {
-		const result = await recoveryTokenModel.deleteOne({ userId });
-		if (result.deletedCount === 0) {
-			throw new DatabaseError(
-				`Error deleting recovery token with userId ${userId}.`
-			);
-		}
+		await recoveryTokenModel.deleteOne({ userId });
 	} catch (error) {
 		throw new DatabaseError(
 			'Error deleting activation token.',
