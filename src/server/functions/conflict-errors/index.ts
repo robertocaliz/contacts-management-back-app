0b857c0ff572c37138a1d62fab215ca2541e9682
robertocaliz <robertocaliz72@gmail.com>
2024-04-08 18:35:04 +0200

    modify conflict error handler

--- src/server/functions/conflict-errors/index.ts
+++ src/server/functions/conflict-errors/index.ts
@@ -1,14 +1,13 @@
 export type ConflictErrorT = {
-    name?: string;
     path?: string;
     message?: string;
 };
 
 let errors: Array<ConflictErrorT> = [];
 
-const setError = (name: string, message: string) => {
+const setError = (path: string, message: string) => {
     errors.push({
-        name,
+        path,
         message,
     });
 };
